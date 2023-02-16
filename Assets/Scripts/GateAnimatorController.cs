2086cdda93496e5a8ad9795c5678d9f11c6e5303
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-02-15 20:52:43 -0500

    added open state to alley gate

---
+++ Assets/Scripts/GateAnimatorController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateAnimatorController : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        animator.SetBool("foundKey", MobsterLevelManager.HasObtainedAlleyKey());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
