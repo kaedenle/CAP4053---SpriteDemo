b1405566a2b9f6bfcd0ce44771fe2bb5c3c9bff5
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-02-16 15:32:00 -0500

    added temp table to hub scene

--- Assets/Scripts/BackgroundFollower.cs
+++ Assets/Scripts/BackgroundFollower.cs
@@ -12,12 +12,13 @@ public class BackgroundFollower : MonoBehaviour
     // the percentage of this object that will remain on screen after a change in player position 
     [Range(0, 100)] public float percentFollow;
 
-    // private variables to keep track of changes
+    // private variables to keep track of position changes
     private float prev_x; 
 
     // Start is called before the first frame update
     void Start()
     {
+        // set the initial player x position
         prev_x = player.transform.position.x;
     }
 
@@ -26,6 +27,9 @@ public class BackgroundFollower : MonoBehaviour
     {
         // calculate the position change and new value
         float delta_x = player.transform.position.x - prev_x;
+
+        if (delta_x == 0) return; // don't want to do any more calculations than we have to
+
         float new_x = transform.position.x + (delta_x * (percentFollow / 100f));
 
         // update the position of this object and record the new x
