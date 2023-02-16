1775899952a2871013804dee671f337ce4305d0b
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-02-16 17:08:34 -0500

    first attempt at item interactable ui highlight

---
+++ Assets/Objects/Custom/Fedora/FedoraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FedoraController : MonoBehaviour
+{
+    [Range(1, 3)] public int startingType = 2;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        animator.SetBool("playerNear", false);
+        animator.SetInteger("id", startingType);
+    }
+
+    public void OnTriggerEnter2D()
+    {
+        animator.SetBool("playerNear", true);
+        Debug.Log("set player near to true");
+    }
+
+    public void OnTriggerExit2D()
+    {
+        animator.SetBool("playerNear", false);
+        Debug.Log("set player near to false");
+    }
+}
