41382397eda53507d2e22c1cf090413bafa44f2c
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-02-20 23:11:41 -0500

    edgecollider->boxcollider, start of lvl transition

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,25 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class LevelLoader : MonoBehaviour
+//{
+//    public Animator transition;
+//    public float transitionTime = 1f;
+
+//    public static void TriggerFade()
+//    {
+
+//    }
+
+//    IEnumerator Fade()
+//    {
+//        // Play animation
+//        transition.SetBool("play", true);
+
+//        // wait
+//        yield return new WaitForSeconds(transitionTime);
+
+//        //load scene
+//    }
+//}
