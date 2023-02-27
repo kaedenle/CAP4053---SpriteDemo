ed2ece6cd52b5700e846bc46d27fb957af427638
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-02-27 13:50:57 -0500

    entity manager script

---
+++ Assets/Scripts/Managers/EntityManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityManager : MonoBehaviour
+{
+    private static bool _movementEnabled;
+
+    public static bool MovementEnabled()
+    {
+        return _movementEnabled;
+    }
+
+    public static void EnableMovement()
+    {
+        _movementEnabled = true;
+    }
+
+    public static void DisableMovement()
+    {
+        _movementEnabled = false;
+    }
+
+    void PlayerDied()
+    {
+        LevelManager.TriggerPlayerDeath();
+    }
+}
