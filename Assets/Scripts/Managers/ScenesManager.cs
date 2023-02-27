c60fc97a1aa7a13e8f09f106a48d00bb42e0e9b6
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-02-27 17:11:56 -0500

    organize objects, renamed child kitchen->bedroom

--- Assets/Scripts/Managers/ScenesManager.cs
+++ Assets/Scripts/Managers/ScenesManager.cs
@@ -20,7 +20,7 @@ public class ScenesManager : MonoBehaviour
         MobsterKitchenDemo,
         MobsterAlleyDemo,
         ChildLivingRoom,
-        ChildKitchen,
+        ChildParentBedroom,
         ChildPlayroom,
         ChildForest
     }
