97b2300aacf6ccde357bae2cf6374c4b7ab69b2e
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-02-28 14:57:07 -0500

    add comments to EntityManager

--- Assets/Scripts/Managers/EntityManager.cs
+++ Assets/Scripts/Managers/EntityManager.cs
@@ -6,21 +6,25 @@ public class EntityManager : MonoBehaviour
 {
     private static bool _movementEnabled;
 
+    // returns whether or not the players and entities should be able to move or interact with the environment
     public static bool MovementEnabled()
     {
         return _movementEnabled;
     }
 
+    // turn movement on
     public static void EnableMovement()
     {
         _movementEnabled = true;
     }
-
+    
+    // turn movement off
     public static void DisableMovement()
     {
         _movementEnabled = false;
     }
 
+    // trigger a player death
     public static void PlayerDied()
     {
         LevelManager.TriggerPlayerDeath();
