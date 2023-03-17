db34dc5f516db59cb88845c1769a3b26b3c720da
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-03-17 11:39:51 -0400

    removing debug stuff

--- Assets/Scripts/AutoenableUI.cs
+++ Assets/Scripts/AutoenableUI.cs
@@ -4,25 +4,16 @@ using UnityEngine;
 
 public class AutoenableUI : MonoBehaviour
 {
-    private UnityEngine.EventSystems.UIBehaviour[] allUI;
     void Awake()
     {
-        allUI = gameObject.GetComponentsInChildren<UnityEngine.EventSystems.UIBehaviour>();
         TurnOn();
     }
 
     void TurnOn()
     {
-        Debug.Log("turning UIs on...");
-        foreach(UnityEngine.EventSystems.UIBehaviour ui in allUI) 
-        { 
-            ui.enabled = true; //turn them on. 
-        } 
-
         foreach(Transform t in GetComponentsInChildren<Transform>(true))
         {
-            t.gameObject.SetActive(true); // = true; //SetActive(true);
-            Debug.Log("testing transform..." + t.ToString());
+            t.gameObject.SetActive(true);
         }
     }
 }
