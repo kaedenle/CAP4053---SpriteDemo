edd8ccdacbf457cf6e02fc59fb3c60f4d3736f31
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-03-19 22:27:47 -0400

    auto-generate boundary around camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,10 +7,32 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     public bool followPlayerX, followPlayerY;
 
+    private float xSize, ySize;
+    private Camera cam;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        cam = UnityEngine.Camera.main;
+        ySize = cam.orthographicSize;
+        xSize = ySize * cam.aspect;         
+
+        int[] dy = {0, 0, 1, -1};
+        int[] dx = {1, -1, 0, 0};
+
+        for(int d = 0; d < 4; d++)
+        {
+            BoxCollider2D bound = gameObject.AddComponent<BoxCollider2D>();
+            
+            Vector2 sz;
+            if(dx[d] == 0)
+                sz = new Vector2(xSize * 2 , 1);
+            else
+                sz = new Vector2(1, ySize * 2);
+
+            bound.offset = new Vector2(dx[d] * xSize + dx[d] * (sz.x / 2F), dy[d] * ySize + dy[d] * (sz.y / 2F));
+            bound.size = sz;
+        }
     }
 
     // Update is called once per frame
