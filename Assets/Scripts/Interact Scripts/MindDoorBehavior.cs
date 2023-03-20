a7f0d5972fdf337079ca0918306526bb91c740b0
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-03-20 17:57:07 -0400

    create backend for holding json interactive txt

--- Assets/Scripts/Interact Scripts/MindDoorBehavior.cs
+++ Assets/Scripts/Interact Scripts/MindDoorBehavior.cs
@@ -7,7 +7,7 @@ public class MindDoorBehavior : Interactive
     new void Start()
     {
         base.Start();
-        outline_thickness = 7.0F;
+        outlineThickness = 7.0F;
         base.GetOutline().SetFloat("_Outline_Thickness", 7.0F);
     }
     new void Update()
