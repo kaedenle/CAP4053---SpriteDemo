2d91aa836405eb22b5763d430436e72ca4f497d0
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-03-20 04:11:38 -0400

    created default aspect ratio (incomplete)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,15 +11,15 @@ public class CameraController : MonoBehaviour
     private Color defaultBackgroundColor = Color.black;
     private float targetAspect = 16.0f / 9.0f;
     private float xSize, ySize;
-    private Camera camera;
+    private Camera cam;
 
     // Start is called before the first frame update
     void Awake()
     {
         // grabs the Main camera. could potentially grab the camera attached to this object
-        camera = UnityEngine.Camera.main;
-        camera.clearFlags = CameraClearFlags.SolidColor;
-        camera.backgroundColor = defaultBackgroundColor;
+        cam = UnityEngine.Camera.main;
+        cam.clearFlags = CameraClearFlags.SolidColor;
+        cam.backgroundColor = defaultBackgroundColor;
         
         // force the camera into our desired aspect ratio
         // determine the game window's current aspect ratio
@@ -31,34 +31,34 @@ public class CameraController : MonoBehaviour
         // if scaled height is less than current height, add letterbox
         if (scaleheight < 1.0f)
         {  
-            Rect rect = camera.rect;
+            Rect rect = cam.rect;
 
             rect.width = 1.0f;
             rect.height = scaleheight;
             rect.x = 0;
             rect.y = (1.0f - scaleheight) / 2.0f;
             
-            camera.rect = rect;
+            cam.rect = rect;
         }
         else // add pillarbox
         {
             float scalewidth = 1.0f / scaleheight;
 
-            Rect rect = camera.rect;
+            Rect rect = cam.rect;
 
             rect.width = scalewidth;
             rect.height = 1.0f;
             rect.x = (1.0f - scalewidth) / 2.0f;
             rect.y = 0;
 
-            camera.rect = rect;
+            cam.rect = rect;
         }
 
         // generate bounding boxes if its checked
         if(generateBoundary)
         {
-            ySize = camera.orthographicSize;
-            xSize = ySize * camera.aspect;         
+            ySize = cam.orthographicSize;
+            xSize = ySize * cam.aspect;         
 
             int[] dy = {0, 0, 1, -1};
             int[] dx = {1, -1, 0, 0};
