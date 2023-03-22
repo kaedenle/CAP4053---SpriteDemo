ec81bf179c7b044485cd147c8d5eefe86bfcf0fd
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-03-21 23:55:01 -0400

    refactor EntityManager (pre-setup for bug fix)

--- Assets/Scripts/Managers/EntityManager.cs
+++ Assets/Scripts/Managers/EntityManager.cs
@@ -1,16 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class EntityManager : MonoBehaviour
 {
-    private static bool _movementEnabled = true, _uiInteractable = true, _envInteractable = true, _attackEnabled = true, _swapEnabled = true, _equipEnabled = true;
+    // private static bool _movementEnabled = true, 
+    //                     _uiInteractable = true, 
+    //                     _envInteractable = true,
+    //                      _attackEnabled = true, 
+    //                      _swapEnabled = true, 
+    //                      _equipEnabled = true;
     private static bool _pause = false;
     private static EntityManager Instance;
+
+    public enum AllStates
+    {
+        _movementEnabled,
+        _uiInteractable,
+        _envInteractable,
+        _attackEnabled,
+        _swapEnabled,
+        _equipEnabled,
+    }
+
+    private static bool[] states; 
+
 
     void Awake()
     {
         Instance = this;
+
+        if(states == null)
+        {
+            states = new bool[System.Enum.GetValues(typeof(AllStates)).Length];
+            Array.Fill(states, true);
+        }
+    }
+
+    public static bool GetState(AllStates state)
+    {
+        return states[(int) state];
+    }
+
+    public static void EnableState(AllStates state)
+    {
+        states[(int) state] = true;
+    }
+
+    public static void DisableState(AllStates state)
+    {
+        states[(int) state] = false;
     }
 
     public static bool IsPaused()
@@ -64,98 +104,103 @@ public class EntityManager : MonoBehaviour
         Unpause();
     }
 
-    public static void EnableSwap()
-    {
-        _swapEnabled = true;
-    }
-    public static void DisableSwap()
-    {
-        _swapEnabled = false;
-    }
-    public static bool SwapEnabled()
-    {
-        return _swapEnabled;
-    }
-    public static void EnableEquip()
-    {
-        _equipEnabled = true;
-    }
-    
-    public static void DisableEquip()
-    {
-        _equipEnabled = false;
-    }
-    public static bool EquipEnabled()
-    {
-        return _equipEnabled;
-    }
-    public static bool MovementEnabled()
-    {
-        return _movementEnabled;
-    }
-
-    // turn movement on
-    public static void EnableMovement()
-    {
-        _movementEnabled = true;
-    }
-    
-    // turn movement off
-    public static void DisableMovement()
-    {
-        _movementEnabled = false;
-    }
-
-    public static bool AttacktEnabled()
-    {
-        return _attackEnabled;
-    }
-
-    // turn attack on
-    public static void EnableAttack()
-    {
-        _attackEnabled = true;
-    }
-    
-    // turn attack off
-    public static void DisableAttack()
-    {
-        _attackEnabled = false;
-    }
-
     // trigger a player death
     public static void PlayerDied()
     {
         LevelManager.TriggerPlayerDeath();
     }
 
-    public static bool IsUIInteractable()
+    public static void EnableSwap()
     {
-        return _uiInteractable;
+        EnableState(AllStates._swapEnabled);
+    }
+
+    public static void DisableSwap()
+    {
+        DisableState(AllStates._swapEnabled);
+    }
+
+    public static bool SwapEnabled()
+    {
+        return GetState(AllStates._swapEnabled);
+    }
+
+    public static void EnableEquip()
+    {
+        EnableState(AllStates._equipEnabled);
+    }
+    
+    public static void DisableEquip()
+    {
+        DisableState(AllStates._equipEnabled);
+    }
+
+    public static bool EquipEnabled()
+    {
+        return GetState(AllStates._equipEnabled);
+    }
+
+    // turn movement on
+    public static void EnableMovement()
+    {
+        EnableState(AllStates._movementEnabled);
+    }
+    
+    // turn movement off
+    public static void DisableMovement()
+    {
+        DisableState(AllStates._movementEnabled);
+    }
+
+    public static bool MovementEnabled()
+    {
+        return GetState(AllStates._movementEnabled);
+    }
+
+    // turn attack on
+    public static void EnableAttack()
+    {
+        EnableState(AllStates._attackEnabled);
+    }
+    
+    // turn attack off
+    public static void DisableAttack()
+    {
+        DisableState(AllStates._attackEnabled);
+    }
+
+    public static bool AttacktEnabled()
+    {
+        return GetState(AllStates._attackEnabled);
     }
 
     public static void EnableUIInteractable()
     {
-        _uiInteractable = true;
+        EnableState(AllStates._uiInteractable);
     }
 
     public static void DisableUIInteractable()
     {
-        _uiInteractable = false;
+        DisableState(AllStates._uiInteractable);
+    }
+
+    public static bool IsUIInteractable()
+    {
+        return GetState(AllStates._uiInteractable);
     }
 
     public static void EnableEnvironmentInteractable()
     {
-        _envInteractable = true;
+        EnableState(AllStates._envInteractable);
     }
 
     public static void DisableEnvironmentInteractable()
     {
-        _envInteractable = false;
+       DisableState(AllStates._envInteractable);
     }
 
     public static bool IsEnvironmentInteractable()
     {
-        return _envInteractable;
+        return GetState(AllStates._envInteractable);
     }
 }
