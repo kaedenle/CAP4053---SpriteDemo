68e944e4b36a821f444262fa99f2b52903ea5a56
Phinpack <111883299+Phinpack@users.noreply.github.com>
2023-04-15 13:11:46 -0400

    Sorting and Dialogue Updates

--- Assets/Scripts/Music/DoNotDestroyMusic.cs
+++ Assets/Scripts/Music/DoNotDestroyMusic.cs
@@ -7,9 +7,12 @@ public class DoNotDestroyMusic : MonoBehaviour
     // Start is called before the first frame update
     private void Awake() 
     {   
-        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("VolumeManager");  
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("VolumeManager");
+        GameObject[] audioObj = GameObject.FindGameObjectsWithTag("LevelTheme"); 
 
         if (musicObj.Length > 1)
+            Destroy(this.gameObject);
+        if (audioObj.Length > 1)
             Destroy(this.gameObject);
 
         DontDestroyOnLoad(this.gameObject); 
