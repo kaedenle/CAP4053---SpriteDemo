9401bd8984e6ae2f5f59e8a6ddc50913a8067042
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2023-04-15 01:25:20 -0400

    weird attacking bug

--- Assets/Scripts/Enemy/AttackAction.cs
+++ Assets/Scripts/Enemy/AttackAction.cs
@@ -15,12 +15,16 @@ namespace BasicEnemy
             Attack(stateMachine);
         }
 
-        IEnumerator Attack(FSM stateMachine)
+        void Attack(FSM stateMachine)
         {
             Debug.Log("got here");
             stateMachine.enemyController.Attack();
             Debug.Log("initial state: " +  stateMachine.enemyController.enabled);
-            yield return new WaitUntil(() => stateMachine.enemyController.enabled);
+            while(!stateMachine.enemyController.enabled)
+            {
+                // do nothing
+            }
+            // yield return new WaitUntil(() => );
             stateMachine.ActionReady = true;
             Debug.Log("finished attacking");
         }
