d9bad497ea53ecd04fc4dc01cbb4d1c0e4efe262
Kaeden Le <kaedenle@gmail.com>
2023-04-16 14:23:45 -0400

    Sound effect slider works with player volume

--- Assets/Kaeden's Experiments (TEMP)/Scripts/AudioController.cs
+++ Assets/Kaeden's Experiments (TEMP)/Scripts/AudioController.cs
@@ -7,9 +7,11 @@ public class AudioController : MonoBehaviour
     private IDictionary<string, AudioManager> audiosrc;
     public void PlayAudioVerbose(AnimationEvent events)
     {
+        float use = 0.5f;
+        if (VolumeManager.instance != null) use = VolumeManager.instance.soundEffectValue;
         if (!audiosrc.ContainsKey(events.stringParameter)) return;
         if (events.intParameter >= audiosrc[events.stringParameter].AttackAudio.Length) return;
-        if (audiosrc[events.stringParameter].AttackAudio != null) audiosrc[events.stringParameter].myaudiosrc.PlayOneShot(audiosrc[events.stringParameter].AttackAudio[events.intParameter], 0.75f);
+        if (audiosrc[events.stringParameter].AttackAudio != null) audiosrc[events.stringParameter].myaudiosrc.PlayOneShot(audiosrc[events.stringParameter].AttackAudio[events.intParameter], use);
     }
     // Start is called before the first frame update
     void Start()
