3e96ab5f22fbf2b11041e1fe0293c4486c6ae8db
Phinpack <111883299+Phinpack@users.noreply.github.com>
2023-04-20 17:01:48 -0400

    Central Hub Music Bug Fix

--- Assets/Scripts/Music/DoNotDestroyMusic.cs
+++ Assets/Scripts/Music/DoNotDestroyMusic.cs
@@ -16,15 +16,21 @@ public class DoNotDestroyMusic : MonoBehaviour
             Destroy(this.gameObject);
         if (audioObj.Length > 1)
             Destroy(this.gameObject);
+        if(SceneManager.GetActiveScene().name == "Central Hub")
+            Destroy(this.gameObject);
 
         DontDestroyOnLoad(this.gameObject); 
     }
     private void Update() 
     {
+        if(SceneManager.GetActiveScene().name == "Central Hub")
+            Destroy(this.gameObject);
+            
         if (SceneManager.GetActiveScene().name == "Child Room")
         {
             m_MyAudioSource.Stop();
         }
+
         else if (SceneManager.GetActiveScene().name == "Living Room" && !m_MyAudioSource.isPlaying)
         {
             m_MyAudioSource.Play();
