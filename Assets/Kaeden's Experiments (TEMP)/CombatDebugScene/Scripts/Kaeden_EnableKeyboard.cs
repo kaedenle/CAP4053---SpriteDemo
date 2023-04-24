1a555147f059c9fa5ca6f6b3e412820c1e2a2d7e
Kaeden Le <kaedenle@gmail.com>
2023-04-24 05:31:20 -0400

    Minor bug fixes

--- Assets/Kaeden's Experiments (TEMP)/CombatDebugScene/Scripts/Kaeden_EnableKeyboard.cs
+++ Assets/Kaeden's Experiments (TEMP)/CombatDebugScene/Scripts/Kaeden_EnableKeyboard.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Kaeden_EnableKeyboard : MonoBehaviour
 {
     public bool KeyboardShortCuts { get; set; }
+    private Toggle tog;
+    private void Awake()
+    {
+        KeyboardShortCuts = WeaponManager.ShortCutCombos;
+        tog = GetComponent<Toggle>();
+        tog.isOn = KeyboardShortCuts;
+    }
     // Update is called once per frame
     void Update()
     {
