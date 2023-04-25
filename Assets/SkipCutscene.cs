502647bc94079100c6c4f5b444b667bb0aca6033
Phinpack <111883299+Phinpack@users.noreply.github.com>
2023-04-25 16:33:01 -0400

    Credits Now Skippable

--- Assets/SkipCutscene.cs
+++ Assets/SkipCutscene.cs
@@ -8,7 +8,9 @@ public class SkipCutscene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && SceneManager.GetActiveScene().name == "StartGameCutscene")
             SceneManager.LoadScene("Central Hub");
+        else if (Input.GetKeyDown(KeyCode.Space) && SceneManager.GetActiveScene().name == "Credits")
+            SceneManager.LoadScene("Menu");
     }
 }
